972225dd23a13e3776b3b4ebf596c179f520ffbf
David Jaworski <jaws360@gmail.com>
2019-10-23 19:41:18 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,12 +6,19 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-end
-
-def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  
+  inner_results = []
+  row_index = 0
+  
+  while row_index < src.count do
+    ingredient1 = src[row_index][0]
+    ingredient2 = src[row_index][1]
+      #add the first and second ingrediants into an array
+      inner_results << "I love #{ingredient1} and #{ingredient2} on my pizza"
+    row_index += 1
+  end
+  puts inner_results
+  return inner_results
 end
 
 def total_even_pairs(src)
@@ -22,4 +29,37 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+
+ results = 0
+  row_index = 0
+  while row_index < src.count do
+      if src[row_index][0]%2 < 1 && src[row_index][1]%2 < 1
+        results += (src[row_index][0] + src[row_index][1])
+      end
+    row_index += 1
+  end
+  return results
 end
+
+
+
+def find_greater_pair(src)
+  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+  # Produce a new Array that contains the larger number of each of the pairs
+  # that are in the inner Arrays
+
+
+  results = []
+  row_index = 0
+  while row_index < src.count do
+      if src[row_index][0] > src[row_index][1]
+        results << src[row_index][0]
+      else
+        results << src[row_index][1]
+      end
+    row_index += 1
+  end
+  return results
+end
+
+
